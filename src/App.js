445bb16be2e377288dbbb79e130043223f958e32
JonasGreim <jonasgreim@gmail.com>
2024-08-25 18:34:08 +0200

    add dropdown to choose dataset add Fortune500 dataset fix warnings fix dataset data

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
-import { Icon, divIcon, point } from "leaflet";
-import {useEffect, useState} from "react";
+import L, { Icon, point } from "leaflet";
+import React, {useEffect, useState} from "react";
 import Slider from 'react-slider';
+import Select from 'react-select';
 
 const customIcon = new Icon({
     iconUrl: require("./icons/placeholder.png"),
     iconSize: [38, 38] // size of the icon
 });
 
+
 const createClusterCustomIcon = function (cluster) {
-    return new divIcon({
+    return L.divIcon({
         html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
         className: "custom-marker-cluster",
         iconSize: point(33, 33, true)
     });
 };
 
+
 export default function App() {
 
+
+    const fortune500Companies = {name: 'Fortune500', path: './fortune500Companies.geojson', yearMin: 1958, yearMax: 2005};
+    const sp500Companies = {name: 'SP500', path: './fortune500Companies.geojson', yearMin: 1980, yearMax: 2024};
+    const dropdownOptions = [
+        { value: fortune500Companies, label: 'Fortune500' },
+        { value: sp500Companies, label: 'SP500' },
+    ]
+
     const [geoJsonData, setGeoJsonData] = useState(null);
-    const [filteredData, setFilteredData] = useState(null);
-    const [selectedYear, setSelectedYear] = useState('1990');
+    const [filteredByYearData, setFilteredByYearData] = useState(null);
+    const [selectedYear, setSelectedYear] = useState(2000);
+    const [selectedDataset, setSelectedDataset] = useState(fortune500Companies);
 
     useEffect(() => {
-        fetch('./company_after1990.geojson')
+        fetch(selectedDataset.path)
             .then(response => response.json())
             .then(data => {
                 setGeoJsonData(data);
@@ -37,50 +49,47 @@ export default function App() {
 
     const filterDataByYear = (data, year) => {
         const filtered = data.features.filter(feature => {
-            const featureYear = new Date(feature.properties.Time).getFullYear();
-            return featureYear === parseInt(year, 10);
+            return feature.properties.year === year;
         });
-        setFilteredData({ ...data, features: filtered });
+        setFilteredByYearData({ ...data, features: filtered });
     };
 
     if (!geoJsonData) {
         return <div>Loading...</div>;
     }
 
-    const handleChange = (value) => {
-        // Find closest mark
-        const closest = marks.reduce((prev, curr) =>
-            Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev
-        );
-        setSelectedYear(closest);
-    }
-
-    const marks = [1990, 1994, 1999, 2004, 2009, 2014, 2019, 2024]
-
     return (
         <div className="viewContainer">
             <div className="uiContainerTop">
                 <h1 className="headline">Company Headquarters in the USA</h1>
             </div>
+            <div className="abc">
+                <Select
+                    className={"dropdown"}
+                    options={dropdownOptions}
+                    defaultValue={dropdownOptions[0]}
+                    placeholder="Select a dataset"
+                    onChange={(selectedOption) => setSelectedDataset(selectedOption?.value)}
+                    menuPlacement="top"
+                />
+                <div className="slider-value">Choose Top50 Company Ranking</div>
+            </div>
             <div className="uiContainerBottom">
                 <Slider
                     className="year-slider"
-                    marks={marks}
                     markClassName="example-mark"
-                    min={1990}
-                    max={2024}
-                    /*step={4}*/ /*bug length of slider changes because its not always 4 steps*/
+                    min={selectedDataset.yearMin}
+                    max={selectedDataset.yearMax}
+                    step={1}
                     thumbClassName="year-slider-thumb"
                     trackClassName="year-slider-track"
                     value={selectedYear}
                     orientation="horizontal"
-                    onChange={handleChange}
+                    onChange={(value) => setSelectedYear(value)}
                 />
                 <div className="slider-value">Selected Year: {selectedYear}</div>
             </div>
-
             <MapContainer center={[37, -95]} zoom={5} className="mapContainer">
-                {/* OPEN STREEN MAPS TILES */}
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -91,17 +100,22 @@ export default function App() {
                     iconCreateFunction={createClusterCustomIcon}
                 >
 
-                    {filteredData && filteredData.features.map((feature, id) => {
+                    {filteredByYearData.features && filteredByYearData.features.map((feature, id) => {
                         const [lng, lat] = feature.geometry.coordinates;
-                        const {"Company Name": companyName, Headquarters, Time} = feature.properties;
+                        const {wikiDataName, qid, revenues, profits, year} = feature.properties;
                         return (
                             <Marker position={[lat, lng]} icon={customIcon} key={id}>
                                 <Popup>
-                                    <strong>{companyName}</strong>
+                                    <strong>{wikiDataName}</strong>
                                     <br/>
-                                    Location: {Headquarters}
+
+                                    <a href={qid} target="_blank">wikidata article</a>
                                     <br/>
-                                    Time: {Time}
+                                    Year: {year}
+                                    <br/>
+                                    Revenues: {revenues} ($ millions)
+                                    <br/>
+                                    Profit: {profits} ($ millions)
                                 </Popup>
                             </Marker>
                         );
