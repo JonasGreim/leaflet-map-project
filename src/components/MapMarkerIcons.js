90f7134db6838750ed0897801b42590d7264a506
JonasGreim <jonasgreim@gmail.com>
2024-08-29 16:55:46 +0200

    - add designed icons - add icons to sidebar - delete old icons

--- src/components/MapMarkerIcons.js
+++ src/components/MapMarkerIcons.js
@@ -1,13 +1,12 @@
 import { Icon } from "leaflet";
 
-
 /*export const customIcon = new Icon({
     iconUrl: require("../icons/placeholder.png"),
     iconSize: [38, 38],
 });*/
 
 export const myIcon = new Icon({
-    iconUrl: require("../icons/test3.png"),
+    iconUrl: require("../images/icons/basic-material.png"),
     iconSize: [45, 45],
 });
 
--- src/components/SidebarPopUp.js
+++ src/components/SidebarPopUp.js
@@ -1,18 +1,43 @@
 import { useState } from "react";
 import '../popUp.css';
+import basicMaterial from './../images/icons/basic-material.png';
+import communications from './../images/icons/communications.png';
+import consumerCyclical from './../images/icons/consumer-cyclical.png';
+import consumerDefense from './../images/icons/consumer-defense.png';
+import energy from './../images/icons/energy.png';
+import financials from './../images/icons/financials.png';
+import health from './../images/icons/health.png';
+import industrials from './../images/icons/industrials.png';
+import informationTechnology from './../images/icons/information-technology.png';
+import utilities from './../images/icons/utilities.png';
+
+export const iconList = [
+    { name: 'Basic Materials', icon: basicMaterial, backend: 'basic_materials' },
+    { name: 'Communications', icon: communications, backend: 'communications' },
+    { name: 'Consumer Cyclical', icon: consumerCyclical, backend: 'consumer_cyclical' },
+    { name: 'Consumer Defensive', icon: consumerDefense, backend: 'consumer_defensive' },
+    { name: 'Energy', icon: energy, backend: 'energy' },
+    { name: 'Financials', icon: financials, backend: 'financials' },
+    { name: 'Health Care', icon: health, backend: 'health' },
+    { name: 'Industrials', icon: industrials, backend: 'industrials' },
+    { name: 'Information Technology', icon: informationTechnology, backend: 'information_technology' },
+    { name: 'Utilities', icon: utilities, backend: 'utilities' }
+];
+
 
 export const SidebarPopUp = () => {
     const [open, setOpen] = useState(true);
-    const list = ['test', 'test2'];
+
     return (
         <div
             className={`sidebar-popup-container ${open ? "sidebar-popup-open" : "sidebar-popup-closed"}`}
         >
             <div className="sidebar-popup-content">
                 <h2>Legend</h2>
-                {list.map((item) => (
-                    <div className="sidebar-popup-item" key={item}>
-                        <p>{item}</p>
+                {iconList.map((item, index) => (
+                    <div className="sidebar-popup-item" key={index}>
+                        <p className="item-name">{item.name}</p>
+                        <img className=".sidebar-popup-item img" src={item.icon} alt={item.name}/>
                     </div>
                 ))}
             </div>
@@ -23,7 +48,7 @@ export const SidebarPopUp = () => {
                     setOpen((prevState) => !prevState);
                 }}
             >
-                Legende {open ? "<" : ">"}
+                Legend {open ? "<" : ">"}
             </div>
         </div>
     );
